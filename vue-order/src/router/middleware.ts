ce6256188c712d66dcfe93a79bc2cd5ddd418fa9
FerdinandoJr <juniorferdinando@gmail.com>
2022-08-26 13:39:48 -0300

    correção imports DefinedRoutes

--- vue-order/src/router/middleware.ts
+++ vue-order/src/router/middleware.ts
@@ -1,16 +1,17 @@
 import UseAuth from "../reactive/useAuth"
+import DefinedRoutes from "./defined-routes"
 
 export const onlyAuthenticated = (to: any, from: any, next: any) => {
   if (UseAuth.isLoggedIn.value) {
     return next()
   }
 
-  return next({ name: 'login' })
+  return next({ name: DefinedRoutes.Login })
 }
 
 export const alreadyLoggedIn = (to: any, from: any, next: any) => {
   if (UseAuth.isLoggedIn.value) {
-    return next({ name: 'dashboard' })
+    return next({ name: DefinedRoutes.Dashboard })
   }
 
   return next()
